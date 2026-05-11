9ecdddee16d07f58533c568e2ce3c7e78eb4ff41
Your Name <youremail@example.com>
2026-05-11 14:36:06 -0400

    Added the Car trait, created getters/setter, and a method 'openTrunk' 'closeTrunk'.

--- src/main/java/com/pluralsight/Car.java
+++ src/main/java/com/pluralsight/Car.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
-public class Car {
+public class Car extends Vehicle {
+    private int numberOfDoors;
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void openTrunk () {
+        System.out.println("Opened trunk.");
+    }
+    public void closeTrunk () {
+        System.out.println("Closed trunk.");
+    }
+
 }
