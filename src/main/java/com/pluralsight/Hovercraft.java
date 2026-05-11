318226f78a28ffeb91bc460cc04e5469dc2dbda3
Your Name <youremail@example.com>
2026-05-11 15:03:55 -0400

    Added the Hovercraft trait, created getters/setter, and a method 'hover'.

--- src/main/java/com/pluralsight/Hovercraft.java
+++ src/main/java/com/pluralsight/Hovercraft.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
-public class Hovercraft {
+public class Hovercraft extends Vehicle {
+    private int airCushionPressure;
+
+    public int getAirCushionPressure() {
+        return airCushionPressure;
+    }
+    public void setAirCushionPressure(int airCushionPressure) {
+        this.airCushionPressure = airCushionPressure;
+    }
+
+    public void hover() {
+        System.out.println("Hovercraft is flying");
+    }
 }
