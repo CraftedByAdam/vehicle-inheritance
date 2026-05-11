dd995a6a9d8dcdc77d133bd248b73f5e6c632737
Your Name <youremail@example.com>
2026-05-11 14:58:36 -0400

    Added the semiTruck trait, created getters/setter, and a method 'loadCargo' 'unloadCargo'.

--- src/main/java/com/pluralsight/SemiTruck.java
+++ src/main/java/com/pluralsight/SemiTruck.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
-public class SemiTruck {
+public class SemiTruck extends Vehicle {
+    private int numberOfTrailers;
+
+    public int getNumberOfTrailers() {
+        return numberOfTrailers;
+    }
+    public void setNumberOfTrailers(int numberOfTrailers) {
+        this.numberOfTrailers = numberOfTrailers;
+    }
+
+    public void loadCargo() {
+        System.out.println("Loaded Cargo");
+    }
+
+    public void unloadCargo() {
+        System.out.println("Unloaded Cargo");
+    }
 }
