7db45ea17edcc03ec979f83f7c0c39108ed1973c
Your Name <youremail@example.com>
2026-05-11 14:18:17 -0400

    Added the Vehicle traits, created getters/setter, and methods to start the engine, stop the engine, accelerate, and brake.

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,68 @@
 package com.pluralsight;
 
 public class Vehicle {
+    private String model;
+    private String color;
+    private int topSpeed;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+    private int fuelCapacity;
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public void startEngine() {
+        System.out.println("Started engine.");
+    }
+
+    public void stopEngine() {
+        System.out.println("Stopped engine.");
+    }
+
+    public void accelerate() {
+        System.out.println("Accelerating.");
+    }
+
+    public void brake() {
+        System.out.println("Braking.");
+    }
 }
